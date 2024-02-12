83164a9d87bb1dc2f093ba234d717d9e742ee314
Huan Zhang <huan.zhang@vizio.com>
2024-02-12 11:20:54 -0600

    Initial Commit

---
+++ UnitConverter002/UnitConverter002App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverter002App.swift
+//  UnitConverter002
+//
+//  Created by Huan Zhang on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverter002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
