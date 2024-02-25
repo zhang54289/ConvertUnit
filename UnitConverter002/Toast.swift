46a51f7e5362b1ea7df2e5a6609b8665d35d4436
HuanZhang <zhang54288@gmail.com>
2024-02-25 00:02:50 -0600

    popup last 3 second.

--- UnitConverter002/Toast.swift
+++ UnitConverter002/Toast.swift
@@ -6,20 +6,32 @@ class Toast: ObservableObject {
     
     static let shared = Toast()
     
+    private var hideTimer: Timer? // Timer to delay hiding of toast
+    
     private init() {}
     
     func showPopup(_ message: String) {
         self.message = message
         isVisible = true
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.isVisible = false
+        hideTimer?.invalidate() // Invalidate previous timer if exists
+        hideTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+            DispatchQueue.main.async {
+                self.isVisible = false
+            }
         }
+    }
+    
+    deinit {
+        hideTimer?.invalidate() // Invalidate timer when the object is deallocated
     }
 }
 
 struct ToastView: View {
     @ObservedObject var toast = Toast.shared
+    
+    // Timer to delay hiding of toast
+    @State private var hideTimer: Timer?
     
     var body: some View {
         if toast.isVisible {
@@ -33,18 +45,26 @@ struct ToastView: View {
                     .padding(.horizontal, 20)
                     .padding(.vertical, 10)
                     .transition(.move(edge: .bottom))
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                            withAnimation {
-                                self.toast.isVisible = false
-                            }
-                        }
-                    }
                     .padding(.bottom, 10)
             }
             .frame(maxWidth: .infinity, alignment: .bottom)
             .edgesIgnoringSafeArea(.all)
             .transition(.opacity)
+            .onAppear {
+                // Invalidate previous timer if exists
+                hideTimer?.invalidate()
+                
+                // Start timer to hide toast after a delay
+                hideTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+                    withAnimation {
+                        self.toast.isVisible = false
+                    }
+                }
+            }
+            .onDisappear {
+                // Invalidate timer when view disappears
+                hideTimer?.invalidate()
+            }
         }
     }
 }
